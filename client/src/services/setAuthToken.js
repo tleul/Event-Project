d84964deea4e45a13d968121cdb335fa06cedcf7
tleul <leult2018@gmail.com>
2020-12-02 00:07:16 -0800

    cors

--- client/src/services/setAuthToken.js
+++ client/src/services/setAuthToken.js
@@ -1,10 +1,10 @@
 import API from './api';
 const setAuthToken = (token) => {
 	if (token) {
-		API.defaults.headers.common['x-auth-token'] = token;
+		API.defaults.headers.common['x-auth-user'] = token;
 		localStorage.setItem('token', token);
 	} else {
-		delete API.defaults.headers.common['x-auth-token'];
+		delete API.defaults.headers.common['x-auth-user'];
 		localStorage.removeItem('token');
 	}
 };
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, x-auth-user',
+		'Origin, X-Requested-With, Content-Type, Accept, x-auth-user,x-auth-token ',
 	);
 	next();
 });
